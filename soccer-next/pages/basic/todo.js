25af8579a2900041c39c85b6dde9bf18d9b89d41
JangWonJong <jwj9922@naver.com>
2022-03-28 17:45:33 +0900

    수업끝 커밋

---
+++ soccer-next/pages/basic/todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Todo(){
+    return (
+        <div className="Todo">
+            <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
+            <label>Todo 컴포턴트 만들기</label>
+        </div>
+    
+    ) 
+}
